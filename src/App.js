49f9cc15b09a763fdee974aa9575b67d990a11c9
bhumill <bhumil.15beceg048@gmail.com>
2021-08-09 15:52:13 -0400

    edit profile created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/Footer.js'
 import Header from './components/Header.js'
+import EditPage from './pages/editProfile.js'
 
 function App() {
   return ( <
     div className = "App" >
     <Header></Header>
+    <EditPage></EditPage>
     <Footer></Footer> 
     </div>
   );
